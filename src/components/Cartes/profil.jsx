d437bb0c435cfdf92e1dbd2e634d476231a1bd52
Thomas-pgrm <141674293+Thomas-pgrm@users.noreply.github.com>
2025-02-04 17:48:28 +0100

    Premier essai fonction rotate 3D style carte de profil

--- src/components/Cartes/profil.jsx
+++ src/components/Cartes/profil.jsx
@@ -1,21 +1,23 @@
-import "../../styles/profil.css"
-import {Link} from 'react-router-dom';
+import "../../styles/profil.css";
+import { Link } from 'react-router-dom';
 
-
-function Profil(){
-    return(
-        <>
-        <div className="card">
-            <img className="card-image" src="../../../certif.jpg" alt=""></img> 
-            <h2 className="card-titre">Mes certifications</h2>
-            <p className="card-texte">Passioné par le développement Web, j&apos;aprécie particulièrement le travail d&apos;équipe pour apprendre et être appris par les autres.</p>
-            <center>
+function Profil() {
+    
+    return (
+        <center>
+            <div className="card">
+                <img className="card-image" src="../../../certif.jpg" alt="" />
+                <h2 className="card-titre">Thomas THONY</h2>
+                <p className="card-texte">
+                    Passioné par le développement Web, j&apos;aprécie particulièrement le travail d&apos;équipe pour apprendre et être appris par les autres.
+                </p>
                 <div className="btn">
-                    <Link to="/certifs"><p>Voir mes certifications</p></Link>
+                    <Link to="/certifs">
+                        <p>Voir mes certifications</p>
+                    </Link>
                 </div>
-            </center>
-        </div>
-        </>
+            </div>
+        </center>
     );
 }
 
