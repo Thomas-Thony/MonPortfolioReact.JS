63b893fa9a96b1aa2560add8bd3c593ae791caab
Thomas-pgrm <141674293+Thomas-pgrm@users.noreply.github.com>
2025-02-04 20:43:14 +0100

    Fin style carte de profil

--- src/components/Cartes/profil.jsx
+++ src/components/Cartes/profil.jsx
@@ -1,24 +1,59 @@
 import "../../styles/profil.css";
+import { useRef, useEffect } from "react";
 import { Link } from 'react-router-dom';
 
 function Profil() {
+  const carteRef = useRef(null);
 
-    return (
-        <center>
-            <div className="carte">
-                <img className="carte-image" src="../../../certif.jpg" alt="" />
-                <h2 className="carte-titre">Thomas THONY</h2>
-                <p className="carte-texte">
-                    Passioné par le développement Web, j&apos;aprécie particulièrement le travail d&apos;équipe pour apprendre et être appris par les autres.
-                </p>
-                <div className="btn">
-                    <Link to="/certifs">
-                        <p>Voir mes certifications</p>
-                    </Link>
-                </div>
-            </div>
-        </center>
-    );
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      if (carteRef.current) {
+        const rect = carteRef.current.getBoundingClientRect();
+        const xCenter = rect.left + rect.width / 2;
+        const yCenter = rect.top + rect.height / 2;
+
+        if (e.clientX < rect.left) {
+          carteRef.current.classList.add("rotate-left");
+          carteRef.current.classList.remove("rotate-right", "rotate-top", "rotate-bottom");
+        } else if (e.clientX > rect.right) {
+          carteRef.current.classList.add("rotate-right");
+          carteRef.current.classList.remove("rotate-left", "rotate-top", "rotate-bottom");
+        } else if (e.clientY < rect.top) {
+          carteRef.current.classList.add("rotate-top");
+          carteRef.current.classList.remove("rotate-left", "rotate-right", "rotate-bottom");
+        } else if (e.clientY > rect.bottom) {
+          carteRef.current.classList.add("rotate-bottom");
+          carteRef.current.classList.remove("rotate-left", "rotate-right", "rotate-top");
+        } else {
+          carteRef.current.classList.remove("rotate-left", "rotate-right", "rotate-top", "rotate-bottom");
+        }
+      }
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <center>
+      <div className="perspective">
+        <div className="carte" ref={carteRef}>
+          <img className="carte-image" src="../../../certif.jpg" alt="Certifications" />
+          <h2 className="carte-titre">Thomas THONY</h2>
+          <p className="carte-texte">
+            Passionné par le développement Web, j&apos;apprécie particulièrement le travail d&apos;équipe pour apprendre et être appris par les autres.
+          </p>
+          <div className="btn">
+            <Link to="/certifs">
+              <p>Voir mes certifications</p>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </center>
+  );
 }
 
 export default Profil;
