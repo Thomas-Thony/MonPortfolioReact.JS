8805652f520b1836dbe951bf5356b6a777fcb268
Thomas-pgrm <141674293+Thomas-pgrm@users.noreply.github.com>
2025-02-07 12:21:28 +0100

    Correctifs de chemin

--- src/components/Certifications/ansii.jsx
+++ src/components/Certifications/ansii.jsx
@@ -4,20 +4,20 @@ import "../../styles/ansii.css";
 function Ansii() {
     return (
         <>
-            <hr/>
-            <p className="Description">
-                Fondée en 2009, l&apos;agence nationale de la sécurité des systèmes d&apos;information (ANSSI) est l&apos;autorité nationale en matière de cybersécurité.
-                Son objectif est de comprendre, prévenir et répondre au risque cyber.
-            </p>
-            <hr/>
-            <br/>
-                <div className="fonctionImage">
-                    <img className='certif_ansii' src="../../public/certfs/Ansii/Certification_Ansii_Thomas-THONY.png" alt="CV de Thomas THONY" />
-                    <div className="millieu">
-                        <div className="text"><a href="../../public/certfs/Ansii/Certification_Ansii_Thomas-THONY.png" download="Certification_ansii_thomas_thony.png">Télécharger</a>
-                        </div>
+        <hr/>
+        <p className="Description">
+            Fondée en 2009, l&apos;agence nationale de la sécurité des systèmes d&aposinformation (ANSSI) est l&apos;autorité nationale en matière decybersécurité.
+            Son objectif est de comprendre, prévenir et répondre au risque cyber.
+        </p>
+        <hr/>
+        <br/>
+            <div className="fonctionImage">
+                <img className='certif_ansii' src="/public/certfs/Ansii/Certification_Ansii_Thomas-THONY.png" alt="CV de Thomas THONY" />
+                <div className="millieu">
+                    <div className="text"><a href="/public/certfs/Ansii/Certification_Ansii_Thomas-THONY.png" download="Certification_ansii_thomas_thony.png">Télécharger</a>
                     </div>
                 </div>
+            </div>
         </>
     );
 }
