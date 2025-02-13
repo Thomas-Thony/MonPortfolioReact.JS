5b17b821bb8b97d985a75ef4e14664a8c0c44bfa
Thomas-pgrm <141674293+Thomas-pgrm@users.noreply.github.com>
2025-02-13 23:30:34 +0100

    Ajout du premier projet Web

--- src/components/Projects/projetsWeb.jsx
+++ src/components/Projects/projetsWeb.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { FaGithub } from "react-icons/fa";
 import "../../styles/projetsWeb.css";
 
 function ProjetsWeb() {
@@ -15,6 +16,29 @@ function ProjetsWeb() {
                 <p>
                     Enfin, le développement full-stack combine les compétences du front-end et du back-end. Un développeur full-stack est capable de travailler sur toutes les couches d&apos;une application, ce qui lui permet de comprendre l&apos;ensemble du processus de création d&apos;un site Web. Cela en fait un atout précieux pour les équipes de développement, car il peut naviguer entre les différentes technologies et résoudre des problèmes de manière efficace.
                 </p>
+            </div>
+            <h2 className="section__title">Mon premier projet web avec des enjeux</h2>
+            <div className="projects__containers container grid">
+                <article className="projects__card">
+                    <a href="https://github.com/Thomas-Thony/Site-Healthy" className="projects__image" target="_blank">
+                        <img src="/public/SourcesProjets/SiteHealthy.png" width="600px" height="auto" alt="Site Healthy" className="projects__img" />
+                    </a>
+                    <div className="projects__data">
+                        <h3 className="projects__name">Site Healthy <img src="/public/SourcesProjets/HealthyFood2.png" alt="Logo Healthy" /></h3>
+                        <p className="projects__description">
+                            Site web de santé et bien-être alimentaire avec des recettes maison, en anglais !
+                        </p>
+                        <div className="projects__skills">
+                            <img src="/public/assets/html.png" alt="Logo HTML 5" className="projects__skill" />
+                            <img src="/public/assets/css-3.png" alt="Logo CSS 3" className="projects__skill" />
+                            <img src="/public/assets/js.png" alt="Logo JavaScript" className="projects__skill"/>
+                        </div>
+                        <a href="https://github.com/Thomas-Thony/Site-Healthy" target="_blank" className="projects__button">
+                            <FaGithub className="i"/>
+                            <span>Voir le projet</span>
+                        </a>
+                    </div>
+                </article>
             </div>
         </>
     );
