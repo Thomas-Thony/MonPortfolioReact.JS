e1dccfe6692234c3f78cf0be6a5266ef20b62113
Thomas-pgrm <141674293+Thomas-pgrm@users.noreply.github.com>
2025-02-16 21:30:26 +0100

    Maj des descriptions des certifs

--- src/components/autresCertifs.jsx
+++ src/components/autresCertifs.jsx
@@ -4,25 +4,34 @@ function AutresCertifs() {
     return (
       <>
         <div className="containerIMG">
-            <div className="galleryDisplay">
+            <div className="gallerieDisplay">
               <a target="_blank" href="/public/certfs/ANSII/Certification_Ansii_Thomas-THONY.png">
                 <img src="/public/certfs/ANSII/Certification_Ansii_Thomas-THONY.png" alt="Certification de l&apos;ANSII" width="500" height="auto"/>
               </a>
-              <div className="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim, natus optio ipsa saepe pariatur sequi porro nemo consequuntur totam ad suscipit cum corporis amet aperiam beatae assumenda, expedita perspiciatis inventore!</div>
+              <div className="desc">
+                <p>Présentation des certifications de l&apos;ANSII.</p>
+                <a className="voirCertifs" href="#">Voir les certifications</a>
+              </div>
             </div>
 
-            <div className="galleryDisplay">
+            <div className="gallerieDisplay">
               <a target="_blank" href="/public/certfs/CNIL/PNG/Certificat_module1_thomas-thony.png">
                 <img src="/public/certfs/CNIL/PNG/Certificat_module1_thomas-thony.png" alt="Certifications de la CNIL" width="600" height="400"/>
               </a>
-              <div className="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim, natus optio ipsa saepe pariatur sequi porro nemo consequuntur totam ad suscipit cum corporis amet aperiam beatae assumenda, expedita perspiciatis inventore!</div>
+              <div className="desc">
+                <p>Présentation des certifications de la CNIL.</p>
+                <a className="voirCertifs" href="#">Voir les certifications</a>
+              </div>
             </div>
 
-            <div className="galleryDisplay">
+            <div className="gallerieDisplay">
               <a target="_blank" href="/public/certfs/DigitalAwarenessUpdate20250214-26-b9qz6z.png">
                 <img src="/public/certfs/DigitalAwarenessUpdate20250214-26-b9qz6z.png" alt="Certifications de Cisco" width="600" height="400"/>
               </a>
-              <div className="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim, natus optio ipsa saepe pariatur sequi porro nemo consequuntur totam ad suscipit cum corporis amet aperiam beatae assumenda, expedita perspiciatis inventore!</div>
+              <div className="desc">
+                <p>Présentation des certifications de Cisco.</p>
+                <a className="voirCertifs" href="#">Voir les certifications</a>
+              </div>
             </div>
           </div>
         </>
