73f823c26074c971fbefc6e6066cb3315f953c6f
Thomas-pgrm <141674293+Thomas-pgrm@users.noreply.github.com>
2025-02-22 22:25:39 +0100

    Maj de la mise en page des liens vers les certififcations

--- src/components/autresCertifs.jsx
+++ src/components/autresCertifs.jsx
@@ -8,31 +8,43 @@ function AutresCertifs() {
         <div className="containerIMG">
             <div className="gallerieDisplay">
               <a target="_blank" href="/certfs/ANSII/Certification_Ansii_Thomas-THONY.png">
-                <img src="/certfs/ANSII/Certification_Ansii_Thomas-THONY.png" alt="Certification de l&apos;ANSII" width="500" height="auto"/>
+                <img src="/certfs/ANSII/Certification_Ansii_Thomas-THONY.png" alt="Certification de l&apos;ANSII"/>
               </a>
               <div className="desc">
                 <p>Présentation des certifications de l&apos;ANSII.</p>
-                <Link to="/ansii" className="voirCertifs">Voir les certifications</Link>
+                <Link to="/ansii" className="voirCertifs">Voir les certifications <GoLinkExternal
+                  className="iconeLien"
+                  onMouseOver={({ target }) => target.style.color = "grey"}
+                  onMouseOut={({ target }) => target.style.color = "white"} />
+                </Link>
               </div>
             </div>
 
             <div className="gallerieDisplay">
               <a target="_blank" href="/public/certfs/CNIL/PNG/Certificat_module1_thomas-thony.png">
-                <img src="/certfs/CNIL/PNG/Certificat_module1_thomas-thony.png" alt="Certifications de la CNIL" width="600" height="400"/>
+                <img src="/certfs/CNIL/PNG/Certificat_module1_thomas-thony.png" alt="Certifications de la CNIL" style={{backgroundColor: "white"}}/>
               </a>
               <div className="desc">
                 <p>Présentation des certifications de la CNIL.</p>
-                <Link to="/cnil" className="voirCertifs">Voir les certifications</Link>
+                <Link to="/cnil" className="voirCertifs">Voir les certifications <GoLinkExternal
+                  className="iconeLien"
+                  onMouseOver={({ target }) => target.style.color = "grey"}
+                  onMouseOut={({ target }) => target.style.color = "white"} />
+                </Link>
               </div>
             </div>
 
             <div className="gallerieDisplay">
-              <a target="_blank" href="/public/certfs/DigitalAwarenessUpdate20250214-26-b9qz6z.png">
-                <img src="/certfs/DigitalAwarenessUpdate20250214-26-b9qz6z.png" alt="Certifications de Cisco" width="600" height="400"/>
+              <a target="_blank" href="/certfs/DigitalAwarenessUpdate20250214-26-b9qz6z.png">
+                <img src="/certfs/DigitalAwarenessUpdate20250214-26-b9qz6z.png" alt="Certifications de Cisco"/>
               </a>
               <div className="desc">
                 <p>Présentation des certifications de Cisco.</p>
-                <Link to="/cisco" className="voirCertifs">Voir les certifications</Link>
+                <Link to="/cisco" className="voirCertifs">Voir les certifications <GoLinkExternal
+                  className="iconeLien"
+                  onMouseOver={({ target }) => target.style.color = "grey"}
+                  onMouseOut={({ target }) => target.style.color = "white"} />
+                </Link>
               </div>
             </div>
           </div>
