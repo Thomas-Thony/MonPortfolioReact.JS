442b6ecdd333b82d24f7b041234e4f01d03dceaa
Thomas-pgrm <141674293+Thomas-pgrm@users.noreply.github.com>
2025-09-29 10:26:52 +0200

    Début ajout expériences professionnelles

--- src/Components/experiences.jsx
+++ src/Components/experiences.jsx
@@ -1,8 +1,24 @@
 import "react";
-//import "./styles/experiences.css";
+import "./styles/experiences.css";
 
 function Experiences() {
-
+    return (
+        <>
+            <div className="container-experiences">
+                <h1>Expériences</h1>
+                <div className="titre-experience">
+                    <h2>Stage de développeur logiciel - Armée de l'air et de l'espace </h2>
+                </div>
+                <div className="date-experience">
+                    <h3>Mai 2025 - Juin 2025</h3>
+                    <h4>2 Mois</h4>
+                </div>
+                <div className="description-experience">
+                    
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Experiences;
