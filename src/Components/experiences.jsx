16e9b81a20f5df3fe54091b2e3cb8feb98ddb57a
Thomas.Th <thomas.thony.69@gmail.com>
2026-06-10 08:44:47 +0200

    [NEW] Ajout du nouveau stage

--- src/Components/experiences.jsx
+++ src/Components/experiences.jsx
@@ -14,7 +14,7 @@ function Experiences() {
                     </div>
                 </div>
                 <div className="date-experience">
-                    <h3>Mai 2025 - Juin 2025</h3>
+                    <h3>Novembre 2025 - Décembre 2025</h3>
                     <h4>2 Mois</h4>
                 </div>
                 <p className="avertissement">
@@ -23,7 +23,7 @@ function Experiences() {
                 <div className="description-experience">
                     <p>
                         Développement d'un logiciel interne de simulation d'opérations aériennes basé sur Unreal Engine 5 (Blueprint et C++).
-                        Mon rôle consistait à convevoir et améliorer les interfaces utilisateurs afin d'avoir une meilleure ésthétique et ergonomie du logiciel.
+                        Mon rôle consistait à améliorer et ajouter des fonctionnalitées pour le drone.
                     </p>  
                 </div>
 
@@ -44,7 +44,50 @@ function Experiences() {
                     </div>
 
                     <div className="image-technologie">
-                        <img src="/assets/github-computer-icons.png"/>
+                        <img src="/assets/visual-studio.png" />
+                        <p className="titre-technologie">Visual Studio</p>
+                    </div>
+                </div>
+                <div className="titre-experience">
+                    <h2>Stage de développeur logiciel - Armée de l'air et de l'espace </h2>
+                    <div className="logo-experience">
+                        <img src="/assets/Logo_de_l'Armée_de_l'Air_et_de_l'Espace.png" alt="Logo Armée de l'air et de l'espace" className="logo-experience" />
+                        <img src="/assets/Logo_CASPOA.png" alt="Logo Ministère des armées" className="logo-experience" />
+                    </div>
+                </div>
+                <hr/>
+                <div className="date-experience">
+                    <h3>Mai 2025 - Juin 2025</h3>
+                    <h4>2 Mois</h4>
+                </div>
+                <p className="avertissement">
+                    Attention ! Toutes les informations présentes dans cet article sont trouvables sur internet <br />(Sauf pour le logiciel sur lequel je travaille mais aucune habilitation n’est requise (NP =&gt; Non Protégé)).
+                </p>
+                <div className="description-experience">
+                    <p>
+                        Développement d'un logiciel interne de simulation d'opérations aériennes basé sur Unreal Engine 5 (Blueprint et C++).
+                        Mon rôle consistait à convevoir et améliorer les interfaces utilisateurs afin d'avoir une meilleure ésthétique et ergonomie du logiciel.
+                    </p>
+                </div>
+
+                <h2 className="titre-image-technologie">Outils utilisés :</h2>
+
+                <div className="images-experience">
+                    <div className="image-technologie">
+                        <img src="/assets/UE5.png" />
+                        <p className="titre-technologie">Unreal Engine 5</p>
+                    </div>
+                    <div className="image-technologie">
+                        <img src="/assets/cpp.png" />
+                        <p className="titre-technologie">C++</p>
+                    </div>
+                    <div className="image-technologie">
+                        <img src="/assets/git.png" />
+                        <p className="titre-technologie">Git</p>
+                    </div>
+
+                    <div className="image-technologie">
+                        <img src="/assets/github-computer-icons.png" />
                         <p className="titre-technologie">Github</p>
                     </div>
 
@@ -59,11 +102,11 @@ function Experiences() {
                 </div>
                 <div className="travaux-experience">
                     <figure className="figure-experience">
-                        <img src="/assets/image-3.png"/>
+                        <img src="/assets/image-3.png" />
                         <figcaption>Interface de controle du drone.</figcaption>
                     </figure>
                     <figure className="figure-experience">
-                        <img src="/assets/image-4.png"/>
+                        <img src="/assets/image-4.png" />
                         <figcaption>Interface de gestion de scénario d'entrainement.</figcaption>
                     </figure>
                 </div>
